42014fb419a36ea6db569a3e53a69831d0c94aee
tzgfryziu <patryk.fryzlewicz@gmail.com>
2024-10-16 11:54:58 +0200

    Integration Limits

--- src/gauss.py
+++ src/gauss.py
@@ -1,7 +1,7 @@
 from math import sqrt
 
 
-def gauss_quadrature_2d(f, p):
+def gauss_quadrature_2d(f, p,a,b,c,d):
 
     if p == 2:
         points = [sqrt(1 / 3), -sqrt(1 / 3)]
@@ -15,16 +15,22 @@ def gauss_quadrature_2d(f, p):
     integral = 0.0
     for i, x in enumerate(points):
         for j, y in enumerate(points):
-            integral += weights[i] * weights[j] * f(x, y)
+            x_mapped = 0.5 * (b - a) * x + 0.5 * (a + b)
+            y_mapped = 0.5 * (d - c) * y + 0.5 * (c + d)
+
+            f_value = f(x_mapped, y_mapped)
+
+            integral += weights[i] * weights[j] * f_value
+
+    integral *= 0.25 * (b - a) * (d - c)
 
     return integral
-
 
 def f1(x, y):
     return -5 * x**2 * y + 2 * x * y + 10
 
 
-integral_2_point = gauss_quadrature_2d(f1, 2)
-integral_3_point = gauss_quadrature_2d(f1, 3)
+integral_2_point = gauss_quadrature_2d(f1, 2,-1,1,-1,1)
+integral_3_point = gauss_quadrature_2d(f1, 3,-1,1,-1,1)
 
 print(integral_2_point, integral_3_point)
